102331afc816bcc63d48bc751852eb89129e47a6
Ben Muzzy <muzz826@gmail.com>
2020-08-14 19:03:00 -0500

    added android support for testing.

---
+++ Just-Lift/android/app/src/main/assets/public/cordova_plugins.js
@@ -0,0 +1,20 @@
+
+  cordova.define('cordova/plugin_list', function(require, exports, module) {
+    module.exports = [
+      {
+          "id": "cordova-plugin-actionsheet.ActionSheet",
+          "file": "plugins/cordova-plugin-actionsheet/www/ActionSheet.js",
+          "pluginId": "cordova-plugin-actionsheet",
+        "clobbers": [
+          "window.plugins.actionsheet"
+        ]
+        }
+    ];
+    module.exports.metadata =
+    // TOP OF METADATA
+    {
+      "cordova-plugin-actionsheet": "2.3.3"
+    };
+    // BOTTOM OF METADATA
+    });
+    
